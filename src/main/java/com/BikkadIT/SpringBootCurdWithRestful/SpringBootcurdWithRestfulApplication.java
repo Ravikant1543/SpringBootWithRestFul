4c767c48226d2fe3bb12b720345d304e40c52429
Ravikant1543 <Ravikantdodke1543@gmail.com>
2022-08-12 01:38:46 +0530

    project added to central repository

---
+++ src/main/java/com/BikkadIT/SpringBootCurdWithRestful/SpringBootcurdWithRestfulApplication.java
@@ -0,0 +1,13 @@
+package com.BikkadIT.SpringBootCurdWithRestful;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootcurdWithRestfulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootcurdWithRestfulApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/BikkadIT/SpringBootCurdWithRestful/SpringBootcurdWithRestfulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BikkadIT.SpringBootCurdWithRestful;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootcurdWithRestfulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
